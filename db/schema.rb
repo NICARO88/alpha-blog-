50af2986e3c3f2cca8da82fe05f7f914db240a60
Mario Concha <profemario.mc@gmail.com>
2025-09-27 01:17:49 -0300

    Add migration to create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_27_000848) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_27_034738) do
   create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
