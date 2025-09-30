79eca162ac53a751a954c1c927ae880209239560
Mario Concha <profemario.mc@gmail.com>
2025-09-29 23:58:00 -0300

    added a show articles feature

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_27_034738) do
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
