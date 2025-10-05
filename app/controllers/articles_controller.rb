84d625754af836d18f7dcc11b6a75f62a0c4756d
Mario Concha <profemario.mc@gmail.com>
2025-10-05 00:56:01 -0300

    Refactor code using partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,49 @@
 class ArticlesController < ApplicationController
-  def show
-    
-    @article = Article.find(params[:id])
-    
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def show  
   end
+    def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was succefully created"
+    redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+    def update
+    if @article.update (article_params)
+      flash[:notice] = "Article was succefully updated"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was succefully deleted"
+    redirect_to articles_path
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+  @article = Article.find(params[:id])
+  end
+
 end
